a7d3755a1990d8790fea5faa9983b831334c09f5
WarBeast26 <gargnishit.2626@gmail.com>
2023-07-07 23:54:47 +0530

    6th commit

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,22 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./intro.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import  Header from "../header/Header";
+import Header from "../header/Header";
 import LeftMe from "../../img/Nishit_PIC-modified.png";
 import Me from "../../img/Me.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CV from "../../Files/Resume_NISHIT-GUPTA.pdf";
+import { BiMenuAltRight } from "react-icons/bi";
+import OutsideClickHandler from "react-outside-click-handler";
+
 const Intro = () => {
+  const [menuOpened, setMenuOpened] = useState(false);  
+  const getMenuStyles = (menuOpened) => {
+    if(document.documentElement.clientWidth <= 1201){
+      return{right: !menuOpened && "-100%"}
+    }
+  }
   React.useEffect(() => {
     AOS.init();
   }, []);
   return (
     <div id="home" className="i">
-     <Header />
+      <Header />
       <div className="i-right">
-        <h2 className="portfolio">PORTFOLIO</h2>
+        <div className="right-upper">
+          <h2 className="portfolio">PORTFOLIO</h2>
+          {/* <div className="menu-btn">
+             <button class="m-btn">
+              <span class="m-icon">
+                <svg viewBox="0 0 175 80" width="40" height="40">
+                  <rect width="80" height="15" fill="#f0f0f0" rx="10"></rect>
+                  <rect
+                    y="30"
+                    width="80"
+                    height="15"
+                    fill="#f0f0f0"
+                    rx="10"
+                  ></rect>
+                  <rect
+                    y="60"
+                    width="80"
+                    height="15"
+                    fill="#f0f0f0"
+                    rx="10"
+                  ></rect>
+                </svg>
+              </span>
+              <span class="text">MENU</span>
+            </button> 
+          </div> */}
+          <div className="menu-icon" on onClick={()=>setMenuOpened((prev)=>!prev)}>
+              <BiMenuAltRight size={30} />
+            </div>
+          <div className="m-container">
+          <OutsideClickHandler
+          onOutsideClick={()=>
+          setMenuOpened(false)}>
+            <div className="m-menu" style={getMenuStyles(menuOpened)}>
+              <a href="">Home</a>
+              <a href="">About</a>
+              <a href="">Skills</a>
+              <a href="">Resume</a>
+              <a href="">Education</a>
+              <a href="">Contact Us</a>
+            </div>
+          </OutsideClickHandler>
+          </div>
+          
+
+        </div>
         <div className="i-right-wrapper">
           <h2 className="i-intro">Hello,I am </h2>
           <h1 className="i-name">Nishit Gupta</h1>
@@ -29,15 +83,13 @@ const Intro = () => {
             </div>
           </div>
           <div class="dwnlod">
-          <a href={CV} download="Resume_Nishit-Gupta">
-          <button className="button button-1">Download CV</button>
-          </a>
-          <a href="#">
-          <button className="button button-2">Hire Me</button>
-          </a>
-          
+            <a href={CV} download="Resume_Nishit-Gupta">
+              <button className="button button-1">Download CV</button>
+            </a>
+            <a href="#">
+              <button className="button button-2">Hire Me</button>
+            </a>
           </div>
-         
         </div>
       </div>
     </div>
