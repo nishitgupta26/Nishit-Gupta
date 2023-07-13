4f8e12dd0e4bf4ccca46cbbdfa948ca985e24ed6
WarBeast26 <gargnishit.2626@gmail.com>
2023-07-13 21:57:21 +0530

    20th

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -16,23 +16,8 @@ const Skills = () => {
         <div class="row skills-content">
           <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 aos-init aos-animate" data-aos="fade-up" data-aos-delay='100' data-aos-duration='500'>
             <div class="progress">
-              <span class="skill">
-                HTML <i class="val">100%</i>
-              </span>
-              <div class="progress-bar-wrap">
-                <div
-                  class="progress-bar"
-                  role="progressbar"
-                  aria-valuenow="100"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                  style={{width: '100%'}}
-                ></div>
-              </div>
-            </div>
-            <div class="progress">
-              <span class="skill">
-                CSS <i class="val">90%</i>
+              <span class="skill">HTML 
+              {/* <i class="val">100%</i> */}
               </span>
               <div class="progress-bar-wrap">
                 <div
@@ -45,10 +30,25 @@ const Skills = () => {
                 ></div>
               </div>
             </div>
+            <div class="progress">
+              <span class="skill">CSS 
+              {/* <i class="val">90%</i> */}
+              </span>
+              <div class="progress-bar-wrap">
+                <div
+                  class="progress-bar"
+                  role="progressbar"
+                  aria-valuenow="86"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style={{width: '86%'}}
+                ></div>
+              </div>
+            </div>
 
             <div class="progress">
-              <span class="skill">
-                JavaScript <i class="val">75%</i>
+              <span class="skill">JavaScript 
+              {/* <i class="val">75%</i> */}
               </span>
               <div class="progress-bar-wrap">
                 <div
@@ -67,8 +67,8 @@ const Skills = () => {
             data-aos="fade-up"
             data-aos-delay='100' data-aos-duration='500'>
             <div class="progress">
-              <span class="skill">
-                C/C++ <i class="val">80%</i>
+              <span class="skill">Problem Solving
+              {/* <i class="val">80%</i> */}
               </span>
               <div class="progress-bar-wrap">
                 <div
@@ -83,8 +83,8 @@ const Skills = () => {
             </div>
 
             <div class="progress">
-              <span class="skill">
-                Java <i class="val">90%</i>
+              <span class="skill">C/C++ 
+              {/* <i class="val">90%</i> */}
               </span>
               <div class="progress-bar-wrap">
                 <div
@@ -99,8 +99,8 @@ const Skills = () => {
             </div>
 
             <div class="progress">
-              <span class="skill">
-                ReactJS <i class="val">55%</i>
+              <span class="skill">ReactJS 
+              {/* <i class="val">55%</i> */}
               </span>
               <div class="progress-bar-wrap">
                 <div
