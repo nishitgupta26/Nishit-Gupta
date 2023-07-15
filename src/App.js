6016f4fc3e530932083de5d5af45b90c0a0c92b0
Vishal Kumar <21ucc116@lnmiit.ac.in>
2023-07-16 02:52:38 +0530

    Loader updated

--- src/App.js
+++ src/App.js
@@ -1,48 +1,38 @@
-import React ,{useState, useEffect} from 'react';
-import ReactDOM from 'react-dom/client';
-import Intro from './components/intro/Intro';
-import About from './components/about/About';
-import Skills from './components/skills/Skills';
-import Resume from './components/resume/Resume';
-import Contact from './components/contact/Contact';
-import HashLoader from "react-spinners/HashLoader";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import Intro from "./components/intro/Intro";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Resume from "./components/resume/Resume";
+import Contact from "./components/contact/Contact";
 import "./app.css";
+import Loading from "./Loading";
+
 
 function App() {
+
   const [loading, setloading] = useState(false);
-  useEffect (() => {
-  setloading (true)
-  setTimeout (() => {
-  setloading(false)
-  }, 2500)
-}, [])
+  useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 3700);
+  }, []);
   return (
     <div className="app">
-    {
-      loading ?
-      <div className="app-loader">
-      <HashLoader
-        color={"#172A8D"}
-        loading={loading}
-        // css={override}
-        size={60}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
-      </div>
-      :
-      <div className="app1">
-      <Intro />
-        <About />
-        <Skills />
-        <Resume />
-        <Contact />
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="app1">
+          <Intro />
+          <About />
+          <Skills />
+          <Resume />
+          <Contact />
         </div>
-    }
-
+      )}
     </div>
   );
 }
 
 export default App;
- 
---
+++ src/Loading.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect, useRef } from "react";
+
+import "./Loading.css";
+
+import { gsap } from "gsap";
+import { Power3 } from "gsap";
+import { TimelineLite } from "gsap/gsap-core.js";
+import { CSSPlugin } from "gsap/CSSPlugin";
+
+
+const Loading = () => {
+    gsap.registerPlugin(CSSPlugin);
+
+  
+  let item = useRef(new Array());
+  const names = ["N", "I", "S", "H", "I", "T", ".", "G", "U", "P", "T", "A"];
+
+  const [tl1] = useState(new TimelineLite({ paused: false }));
+
+  useEffect(() => {
+    tl1.to(item.current, {
+          y: 0,
+          stagger: 0.1,
+          delay: 0.05,
+          duration: 0.05,
+        },"a")
+    .to(item.current, {
+      y: "-14vw",
+      stagger: 0.1,
+      delay: 1,
+      duration: 0.05,
+    },"b");
+  }, []);
+
+  return (
+    <div className="header"  >
+          <div
+            className="myText"
+          >
+            {names.map((name,index)=>(
+            <div key={index} className={`char ${(name==='.')&&"dot"}`} ref={(el) => {
+                item.current.push(el);
+              }}>
+              {name}
+            </div>
+          ))}
+          </div>
+        </div>
+  )
+}
+
+export default Loading
