0ad628e571e6554078b3901db84676866f29ed88
Vishal Kumar <21ucc116@lnmiit.ac.in>
2024-02-28 12:54:54 +0530

    android config

--- src/App.js
+++ src/App.js
@@ -6,9 +6,41 @@ import Resume from "./components/resume/Resume";
 import Contact from "./components/contact/Contact";
 import "./app.css";
 import Loading from "./Loading";
+
 import LocomotiveScroll from "locomotive-scroll";
+import { PushNotifications } from '@capacitor/push-notifications';
 
 function App() {
+  useEffect(() => {
+    const setupPushNotifications = async () => {
+        // Request permission for push notifications
+        await PushNotifications.requestPermissions();
+
+        // Register for push notifications
+        PushNotifications.addListener('registration', (token) => {
+            console.log('Push token:', token.value);
+            // Save the token to your server or use it as needed
+        });
+
+        // Handle received notifications
+        PushNotifications.addListener('pushNotificationReceived', (notification) => {
+            console.log('Push notification received:', notification);
+            // Handle the notification data (e.g., show an alert, navigate to a specific page)
+        });
+
+        // Handle tap on notification
+        PushNotifications.addListener('pushNotificationActionPerformed', (action) => {
+            console.log('Push notification action performed:', action);
+            // Handle the action (e.g., navigate to a specific page)
+        });
+    };
+
+    setupPushNotifications();
+}, []);
+
+
+
+
   const ap1 = useRef();
 
   // const [loading, setloading] = useState(false);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import 'firebase/messaging';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAt5FpnXQXzZa8hj_YaReIUwBgW11YdZKM",
+  authDomain: "test-noti-dbae3.firebaseapp.com",
+  projectId: "test-noti-dbae3",
+  storageBucket: "test-noti-dbae3.appspot.com",
+  messagingSenderId: "386458624453",
+  appId: "1:386458624453:web:43793f1a47950e0125ebac"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,3 +28,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
